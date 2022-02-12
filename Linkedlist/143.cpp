a6fe9f44570364030f67939101f15c550727012f
Ronit <ronitkhalate234@gmail.com>
2022-02-12 22:31:31 +0530

    restored the linkedlist as it is before returning. to check if list is palindrome or not we reversed the linkedlist from mid->next so we restored the list before returing

--- Linkedlist/143.cpp
+++ Linkedlist/143.cpp
@@ -1,6 +1,6 @@
 
 // ? Check if Linked List is Palindrome
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
@@ -16,20 +16,20 @@ class Linkedlist
 public:
     void create(vector<int>);
     bool isPalindrome();
-    Node*find_mid();
-    Node* reverse(Node*);
+    Node *find_mid();
+    Node *reverse(Node *);
 };
 int main()
 {
     Linkedlist a;
-    vector<int> arr= {1,2,3,1,1};
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     a.create(arr);
-    cout<<a.isPalindrome();
+    cout << a.isPalindrome();
 
     return 0;
 }
 
-void Linkedlist ::create(vector<int>arr)
+void Linkedlist ::create(vector<int> arr)
 {
     Node *temp;
     if (!head)
@@ -52,48 +52,48 @@ void Linkedlist ::create(vector<int>arr)
 
 bool Linkedlist ::isPalindrome()
 {
-    Node*mid=find_mid();
-    mid->next=reverse(mid->next);
-    Node*a=head,*b=mid->next;
+    Node *mid = find_mid();
+    mid->next = reverse(mid->next);
+    Node *a = head, *b = mid->next;
 
     while (b)
     {
-        if(a->data!=b->data)
+        if (a->data != b->data)
         {
+            mid->next = reverse(mid->next);
             return false;
         }
-        a=a->next;
-        b=b->next;
+        a = a->next;
+        b = b->next;
     }
+    mid->next = reverse(mid->next);
     return true;
-    
-
 }
 
-Node* Linkedlist ::find_mid()
+Node *Linkedlist ::find_mid()
 {
-    Node*slow=head,*fast=head->next;
+    Node *slow = head, *fast = head->next;
 
     while (fast && fast->next)
     {
-        fast=fast->next->next;
-        slow=slow->next;
+        fast = fast->next->next;
+        slow = slow->next;
     }
-    
+
     return slow;
 }
 
-Node* Linkedlist ::reverse(Node*start)
+Node *Linkedlist ::reverse(Node *start)
 {
-    Node*a=NULL,*b=NULL,*c=start;
+    Node *a = NULL, *b = NULL, *c = start;
 
     while (c)
     {
-        a=b;
-        b=c;
-        c=c->next;
-        b->next=a;
+        a = b;
+        b = c;
+        c = c->next;
+        b->next = a;
     }
-    
+
     return b;
 }
